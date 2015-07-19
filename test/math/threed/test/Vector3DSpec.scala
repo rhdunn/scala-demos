fece72ee4cef60c145347ae9abc6ab42c63afc19
Reece H. Dunn <msclrhd@gmail.com>
2015-07-19 08:51:40 +0100

    Create a simple Vector3D class.

---
+++ test/math/threed/test/Vector3DSpec.scala
@@ -0,0 +1,33 @@
+/** 3D Vector test file
+  *
+  * Copyright (C) 2015 Reece H. Dunn
+  *
+  * This file is part of scala-demos.
+  *
+  * scala-demos is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * scala-demos is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with scala-demos.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package math.threed.test
+
+import math.threed._
+import org.scalatest._
+
+class Vector3DSpec extends FlatSpec {
+  "Initialization" should "initialize x, y and z correctly" in {
+    val v = new Vector3D(1.2, 2.8, -5.6)
+    assert(v.x ==  1.2)
+    assert(v.y ==  2.8)
+    assert(v.z == -5.6)
+  }
+}
