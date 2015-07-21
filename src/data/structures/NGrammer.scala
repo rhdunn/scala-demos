2f97b3cfb9b6460101d7ab5092a49447e1007191
Reece H. Dunn <msclrhd@gmail.com>
2015-07-21 11:18:36 +0100

    Create an ngram algorithm.

---
+++ src/data/structures/NGrammer.scala
@@ -0,0 +1,28 @@
+/** NGrammer : n-gram construction algorithm
+  *
+  * Copyright (C) 2015 Reece H. Dunn
+  *
+  * This file is part of scala-demos.
+  *
+  * scala-demos is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * scala-demos is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with scala-demos.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package data.structures
+
+object NGrammer {
+  def ngrams[T](items: Iterable[T], arity: Int) : Iterator[Iterable[T]] = {
+    if (items.size < arity) return Iterator.empty
+    items.sliding(arity)
+  }
+}
---
+++ test/data/structures/test/NGrammerSpec.scala
@@ -0,0 +1,66 @@
+/** NGrammer : n-gram construction algorithm tests
+  *
+  * Copyright (C) 2015 Reece H. Dunn
+  *
+  * This file is part of scala-demos.
+  *
+  * scala-demos is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * scala-demos is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with scala-demos.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package data.structures.test
+
+import data.structures._
+import org.scalatest._
+
+class NGrammerSpec extends FlatSpec {
+  "2-gram" should "not return items for a 0 element sequence" in {
+    assert(NGrammer.ngrams(List(), 2).isEmpty)
+  }
+
+  "2-gram" should "not return items for a 1 element sequence" in {
+    assert(NGrammer.ngrams(List(9), 2).isEmpty)
+  }
+
+  "2-gram" should "return one bigram for a 2 element sequence" in {
+    val ret = NGrammer.ngrams(List(9, 8), 2).toList
+    assert(ret == List(List(9, 8)))
+  }
+
+  "2-gram" should "return the bigrams in an integer sequence" in {
+    val ret = NGrammer.ngrams(List(1, 2, 3, 4, 5), 2).toList
+    assert(ret == List(List(1, 2), List(2, 3), List(3, 4), List(4, 5)))
+  }
+
+  "3-gram" should "not return items for a 0 element sequence" in {
+    assert(NGrammer.ngrams(List(), 3).isEmpty)
+  }
+
+  "3-gram" should "not return items for a 1 element sequence" in {
+    assert(NGrammer.ngrams(List(9), 3).isEmpty)
+  }
+
+  "3-gram" should "not return items for a 2 element sequence" in {
+    assert(NGrammer.ngrams(List(9, 8), 3).isEmpty)
+  }
+
+  "3-gram" should "return one trigram for a 3 element sequence" in {
+    val ret = NGrammer.ngrams(List(9, 8, 7), 3).toList
+    assert(ret == List(List(9, 8, 7)))
+  }
+
+  "3-gram" should "return the trigrams in an integer sequence" in {
+    val ret = NGrammer.ngrams(List(1, 2, 3, 4, 5), 3).toList
+    assert(ret == List(List(1, 2, 3), List(2, 3, 4), List(3, 4, 5)))
+  }
+}
